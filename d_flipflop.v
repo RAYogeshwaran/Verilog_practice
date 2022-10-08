3342873af9f79ccd769a5ef6c35bee1416a92167
RAYogeshwaran <56624086+RAYogeshwaran@users.noreply.github.com>
2022-10-08 13:49:28 +0530

    Create d_flipflop.v

---
+++ d_flipflop.v
@@ -0,0 +1,13 @@
+module d_flipflop(D,clk,reset,Q);
+input D; // Data input 
+input clk; // clock input 
+input reset; // synchronous reset 
+output reg Q; // output Q 
+always @(posedge clk) 
+begin
+ if(reset==1'b1)
+  Q <= 1'b0; 
+ else 
+  Q <= D; 
+end 
+endmodule 
